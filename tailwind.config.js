85b99ac6e24fbc441db7b0a6d687785b114e7fd2
aixion1506 <aixion1506@gmail.com>
2024-08-27 21:49:48 +0900

    Add: libraries

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 이 부분을 추가하여 Tailwind CSS가 사용할 파일을 지정
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
