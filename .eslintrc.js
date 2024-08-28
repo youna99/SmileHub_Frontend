0f83fd26e3aa016dbd9b90cb823c4d1ea630ca51
youna <dbsk0108@gmail.com>
2024-08-28 18:34:56 +0900

    Error sol : prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
     ],
     plugins: ['prettier'], // Prettier 플러그인 추가
     rules: {
-        'prettier/prettier': 'error', // Prettier 규칙을 ESLint 오류로 설정
-        // 추가적인 규칙을 여기에 정의할 수 있습니다.
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
     },
 };
