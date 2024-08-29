cdc9470b848c6cf65bd5e92f6d42d905d03e82d0
youna <dbsk0108@gmail.com>
2024-08-29 14:19:43 +0900

    correct: prettierrc, 라이브러리 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
+const flowbite = require('flowbite-react/tailwind');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // 이 부분을 추가하여 Tailwind CSS가 사용할 파일을 지정
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}', // 이 부분을 추가하여 Tailwind CSS가 사용할 파일을 지정
+        flowbite.content(),
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [flowbite.plugin()],
+};
