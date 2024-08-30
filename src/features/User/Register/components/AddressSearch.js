935179e6420fd04799e35a5462085eab99b9987b
youna <dbsk0108@gmail.com>
2024-08-30 23:56:53 +0900

    feat: 주소 api 로직 component

--- src/features/User/Register/components/AddressSearch.js
+++ src/features/User/Register/components/AddressSearch.js
@@ -5,30 +5,56 @@ import { useDispatch } from 'react-redux';
 import { setUserField } from '../../store/userSlice';
 
 // 주소 api
-const AddressSearch = ({ isOpen, onRequestClose }) => {
+const AddressSearch = ({ isOpen, onRequestClose, setValue }) => {
   const dispatch = useDispatch();
 
   const handleComplete = (data) => {
-    let fullAddress = data.address;
-    let extraAddress = '';
+    console.log('data =====>>', data);
+    let addr = '';
+    let extraAddr = '';
+    let sido = data.sido;
+    let sigungu = data.sigungu;
+    let bname = data.bname;
 
-    if (data.addressType === 'R') {
-      if (data.bname !== '') {
-        extraAddress += data.bname;
-      }
-      if (data.buildingName !== '') {
-        extraAddress +=
-          extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
-      }
-      fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
+    if (data.userSelectedType === 'R') {
+      addr = data.roadAddress;
+    } else {
+      addr = data.jibunAddress;
     }
+
+    if (data.userSelectedType === 'R') {
+      if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+        extraAddr += data.bname;
+      }
+      if (data.buildingName !== '' && data.apartment === 'Y') {
+        extraAddr +=
+          extraAddr !== '' ? ', ' + data.buildingName : data.buildingName;
+      }
+      if (extraAddr !== '') {
+        extraAddr = ' (' + extraAddr + ')';
+      }
+    }
+    console.log('addr >>>>', addr);
+    console.log('extraAddr >>', extraAddr);
+    console.log('extraAddr >>', sido);
+    console.log('extraAddr >>', sigungu);
+    console.log('extraAddr >>', bname);
 
     // Redux 상태 업데이트
     dispatch(setUserField({ field: 'address.postcode', value: data.zonecode }));
-    dispatch(setUserField({ field: 'address.address', value: fullAddress }));
-    dispatch(
-      setUserField({ field: 'address.extraAddress', value: extraAddress }),
-    );
+    dispatch(setUserField({ field: 'address.address', value: addr }));
+    dispatch(setUserField({ field: 'address.extraAddress', value: extraAddr }));
+    dispatch(setUserField({ field: 'address.sido', value: sido }));
+    dispatch(setUserField({ field: 'address.sigungu', value: sigungu }));
+    dispatch(setUserField({ field: 'address.bname', value: bname }));
+
+    // react-hook-form 상태 업데이트
+    setValue('address.postcode', data.zonecode);
+    setValue('address.address', addr);
+    setValue('address.extraAddress', extraAddr);
+    setValue('address.sido', sido);
+    setValue('address.sigungu', sigungu);
+    setValue('address.bname', bname);
 
     onRequestClose();
   };
