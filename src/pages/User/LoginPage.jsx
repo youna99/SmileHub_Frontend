2d97e09d51fe5a39aa3104cc928bc215c5294497
youna <dbsk0108@gmail.com>
2024-09-01 01:25:37 +0900

    style: loginPage UI

--- src/pages/User/LoginPage.jsx
+++ src/pages/User/LoginPage.jsx
@@ -1,32 +1,47 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Button, Label, TextInput } from 'flowbite-react';
 
-export default function LoginPage() {
-  const navigate = useNavigate();
+export default function LoginPage({
+  navigate,
+  handleSubmit,
+  setEmail,
+  setPassword,
+  error,
+}) {
   return (
     <>
       <div className="flex justify-between items-center px-5 py-3 bg-gray-100">
         <button onClick={() => navigate(-1)}>뒤로가기</button>
       </div>
       <div className="flex justify-center items-center min-h-screen px-4">
-        <form className="flex flex-col gap-4 w-full max-w-lg mx-auto">
+        <form
+          className="flex flex-col gap-4 w-full max-w-lg mx-auto"
+          onSubmit={handleSubmit}
+        >
           <div>
             <div className="mb-2 block">
-              <Label htmlFor="email1" value="아이디(이메일)" />
+              <Label htmlFor="email" value="아이디(이메일)" />
             </div>
             <TextInput
-              id="email1"
+              id="email"
               type="email"
               placeholder="example@naver.com"
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
           <div>
             <div className="mb-2 block">
-              <Label htmlFor="password1" value="비밀번호" />
+              <Label htmlFor="password" value="비밀번호" />
             </div>
-            <TextInput id="password1" type="password" required />
+            <TextInput
+              id="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            {error && <div className="text-red-500 text-sm">{error}</div>}
           </div>
           <Button type="submit">로그인</Button>
           <div className="flex justify-end">
