92a1a04e268cd0fcbed3e8b91c5733c56416d44f
aixion1506 <aixion1506@gmail.com>
2024-09-01 01:52:20 +0900

    Rename: PostProductPage Routing

--- src/app/rootRouter.js
+++ src/app/rootRouter.js
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from '../pages/User/LoginPage';
 import MainPage from '../pages/MainPage';
-import PostPruductPage from '../pages/Product/PostProductPage';
+import PostProductPage from '../pages/Product/PostProduct/PostProductPage';
 
 export const RootRouter = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/" element={<MainPage />} />
-      <Route path="/postproduct" element={<PostPruductPage />} />
+      <Route path="/postproduct" element={<PostProductPage />} />
     </Routes>
   );
 };
