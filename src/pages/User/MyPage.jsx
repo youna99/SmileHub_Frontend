42ac543fd274cd57e09db7ca0fa1357b68f53553
youna <dbsk0108@gmail.com>
2024-09-02 01:11:33 +0900

    style: 회원정보수정 ui

--- src/pages/User/MyPage.jsx
+++ src/pages/User/MyPage.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Avatar, Tabs } from 'flowbite-react';
-import '../../features/User/store/userSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import UserEditContainer from '../../features/User/mypage/container/UserEditContainer';
 
 const MyPage = () => {
   const currentUser = useSelector((state) => state.user.currentUser);
-  const dispatch = useDispatch();
   return (
     <>
       <main className="flex justify-center py-10 w-full">
         <div className="flex items-center space-x-4 w-full max-w-3xl border p-6 bg-gray-200 mx-auto">
-          <Avatar img={`${process.env.PUBLIC_URL}/포차코 공룡.jpg`} size="xl" />
+          <Avatar img="" size="xl" />
           <div className="space-y-2 pl-8">
             <div className="text-2xl font-bold">{currentUser.nickname}</div>
             <div className="text-lg text-gray-700">{currentUser.temp}</div>
@@ -49,12 +48,7 @@ const MyPage = () => {
               visibility and styling.
             </Tabs.Item>
             <Tabs.Item title="내 정보 수정">
-              {/* <span className="font-medium text-gray-800 dark:text-white">
-                Contacts tab's associated content
-              </span>
-              . Clicking another tab will toggle the visibility of this one for
-              the next. The tab JavaScript swaps classes to control the content
-              visibility and styling. */}
+              <UserEditContainer />
             </Tabs.Item>
           </Tabs>
         </div>
