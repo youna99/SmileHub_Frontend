8bee12c80ac706758ba6c1f9c909bc2ddac9ba9e
youna <dbsk0108@gmail.com>
2024-09-03 00:58:45 +0900

    feat: mypageEdit router 추가

--- src/app/rootRouter.js
+++ src/app/rootRouter.js
@@ -3,10 +3,12 @@ import MainPage from '../pages/MainPage';
 import RegisterContainer from '../features/User/Register/container/RegisterContainer';
 import MyPage from '../pages/User/MyPage';
 import { LoginContainer } from '../features/User/login/container/LoginContainer';
+import UserEditContainer from '../features/User/mypage/container/UserEditContainer';
 
 export const RootRouter = () => {
   return (
     <Routes>
+      <Route path="/mypageEdit" element={<UserEditContainer />} />
       <Route path="/register" element={<RegisterContainer />} />
       <Route path="/login" element={<LoginContainer />} />
       <Route path="/mypage" element={<MyPage />} />
