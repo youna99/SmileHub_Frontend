c216ff6ba0c6489eeaa768a1d08ac47efcfc1f67
youna <dbsk0108@gmail.com>
2024-09-03 22:43:21 +0900

    feat: 판매내역, 구매내역 거래상태에 따른 기능 구현

--- src/features/User/mypage/components/UserBuyList.jsx
+++ src/features/User/mypage/components/UserBuyList.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
+import { Button } from 'flowbite-react';
+import { useDispatch } from 'react-redux';
+import { updateBuysStatus, updateSellsStatus } from '../../store/myPageSlice';
 import UserProductsList from './UserProductsList';
+// import UserProductsList from './UserProductsList';
 
-const UserBuyList = () => {
-  return (
-    <UserProductsList
-      productName="닌텐도 스위치"
-      price="61,800"
-      address="서울시 강남구"
-      status="배송 중"
-      isSold={true}
-      imgSrc="product_image_url"
-    />
+const UserBuyList = ({ buy }) => {
+  const dispatch = useDispatch();
+
+  // 구매자: 상품 확인 완료 -> 정산 완료
+  const handleConfirmDelivery = (productId) => {
+    dispatch(updateSellsStatus({ productId, newStatus: '정산 완료' }));
+    dispatch(updateBuysStatus({ productId, newStatus: '정산 완료' }));
+  };
+
+  // 구매자: 거절 -> 환불완료
+  const handleRejectBuy = (productId) => {
+    dispatch(updateBuysStatus({ productId, newStatus: '환불 완료' }));
+    dispatch(updateSellsStatus({ productId, newStatus: '환불 완료' }));
+  };
+
+  const buttons = (
+    <>
+      {buy.status === '배송 완료' && (
+        <>
+          <Button
+            color="gray"
+            className="mr-2"
+            onClick={() => handleConfirmDelivery(buy.productId)}
+          >
+            상태확인 완료
+          </Button>
+          <Button color="gray" onClick={() => handleRejectBuy(buy.productId)}>
+            거절
+          </Button>
+        </>
+      )}
+    </>
   );
+
+  return <UserProductsList product={buy} buttons={buttons} />;
 };
 
 export default UserBuyList;
--- src/features/User/mypage/components/UserSellList.jsx
+++ src/features/User/mypage/components/UserSellList.jsx
@@ -1,17 +1,64 @@
 import React from 'react';
+import { Button } from 'flowbite-react';
+import { useDispatch } from 'react-redux';
+import { updateSellsStatus, updateBuysStatus } from '../../store/myPageSlice';
 import UserProductsList from './UserProductsList';
 
-const UserSellList = () => {
-  return (
-    <UserProductsList
-      productName="닌텐도 스위치"
-      price="61,800"
-      address="서울시 강남구"
-      status="배송 중"
-      isSold={false}
-      imgSrc="product_image_url"
-    />
+const UserSellList = ({ sell }) => {
+  const dispatch = useDispatch();
+
+  // 판매자: 결제 후 수락 -> 배송 대기중
+  const handleAcceptSell = (productId) => {
+    dispatch(updateSellsStatus({ productId, newStatus: '배송 대기중' }));
+  };
+
+  // 판매자: 거절 -> 다시 판매중
+  const handleRejectSell = (productId) => {
+    dispatch(updateSellsStatus({ productId, newStatus: '판매중' }));
+  };
+
+  // 판매자: 발송 완료 -> 배송중
+  const handleCompleteSell = (productId) => {
+    dispatch(updateSellsStatus({ productId, newStatus: '배송중' }));
+    dispatch(updateBuysStatus({ productId, newStatus: '배송중' }));
+
+    // 10초 후 배송중 상태가 배송완료로 변경
+    setTimeout(() => {
+      dispatch(updateSellsStatus({ productId, newStatus: '배송 완료' }));
+      dispatch(updateBuysStatus({ productId, newStatus: '배송 완료' }));
+    }, 10000);
+  };
+
+  const buttons = (
+    <>
+      {sell.status === '판매중' && sell.isPayment ? (
+        <>
+          <Button
+            color="gray"
+            className="mr-2"
+            onClick={() => handleAcceptSell(sell.productId)}
+          >
+            수락
+          </Button>
+          <Button color="gray" onClick={() => handleRejectSell(sell.productId)}>
+            거절
+          </Button>
+        </>
+      ) : (
+        sell.status === '배송 대기중' && (
+          <Button
+            color="gray"
+            className="mr-2"
+            onClick={() => handleCompleteSell(sell.productId)}
+          >
+            발송 완료
+          </Button>
+        )
+      )}
+    </>
   );
+
+  return <UserProductsList product={sell} buttons={buttons} />;
 };
 
 export default UserSellList;
