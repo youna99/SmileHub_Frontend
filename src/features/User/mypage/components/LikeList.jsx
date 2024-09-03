c891af8dce8d6311d91ef618ae660f60a9891d87
youna <dbsk0108@gmail.com>
2024-09-03 23:28:59 +0900

    feat: 찜목록 기능(판매완료상태 표시, 찜 취소)

--- src/features/User/mypage/components/LikeList.jsx
+++ src/features/User/mypage/components/LikeList.jsx
@@ -1,47 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar } from 'flowbite-react';
 
 const LikeList = () => {
-  const products = [
+  const initialProducts = [
     {
-      imgSrc: 'your-image-url-1',
+      userid: '1',
+      productId: '1',
+      imgSrc: '',
       title: '상품1',
       price: '50,000',
       location: '주소',
       isSoldOut: true,
+      isLike: true,
     },
     {
-      imgSrc: 'your-image-url-2',
+      userid: '1',
+      productId: '2',
+      imgSrc: '',
       title: '상품2',
       price: '30,000',
       location: '주소',
       isSoldOut: false,
+      isLike: true,
     },
     {
-      imgSrc: 'your-image-url-3',
+      userid: '1',
+      productId: '3',
+      imgSrc: '',
       title: '상품3',
       price: '20,000',
       location: '주소',
       isSoldOut: false,
+      isLike: true,
     },
     {
-      imgSrc: 'your-image-url-3',
+      userid: '1',
+      productId: '4',
+      imgSrc: '',
       title: '상품3',
       price: '20,000',
       location: '주소',
       isSoldOut: true,
+      isLike: true,
+    },
+    {
+      userid: '1',
+      productId: '5',
+      imgSrc: '',
+      title: '상품4',
+      price: '50,000',
+      location: '주소',
+      isSoldOut: false,
+      isLike: true,
     },
   ];
 
+  const [likeProducts, setLikeProducts] = useState(initialProducts); // 찜 목록 상태 관리
+
+  const deleteLike = (productId) => {
+    setLikeProducts(
+      likeProducts.filter((product) => product.productId !== productId),
+    );
+  };
+
   return (
     <section className="flex flex-wrap">
-      {products.map((product, index) => (
-        <div key={index} className="p-2 w-full sm:w-1/2">
+      {likeProducts.map((product) => (
+        <div key={product.productId} className="p-2 w-full sm:w-1/2">
           <div className="relative flex items-center bg-white p-4 rounded-lg border border-gray-200">
             <img
               src="/images/likeY.png"
               alt="Like"
-              className="absolute top-2 right-2 w-6 h-6"
+              className="absolute top-2 right-2 w-6 h-6 cursor-pointer"
+              onClick={() => deleteLike(product.productId)}
             />
             <Avatar
               img={product.imgSrc}
