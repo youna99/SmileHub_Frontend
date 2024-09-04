560047f6462cc7069d52834d1b700d3c040ed243
youna <dbsk0108@gmail.com>
2024-09-05 01:25:57 +0900

    fix: userReducer import 누락 수정

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import userReducer from '../features/User/store/userSlice';
 import productReducer from '../features/Product/store/proudctSlice';
 import mypageReducer from '../features/User/store/myPageSlice';
 
