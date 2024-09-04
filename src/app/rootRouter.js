21b6917d3e28b73ef03aaee04b939e3a6836e1a6
aixion1506 <aixion1506@gmail.com>
2024-09-04 18:52:37 +0900

    Feat: PageNotFound

--- src/app/rootRouter.js
+++ src/app/rootRouter.js
@@ -6,12 +6,14 @@ import UserEditContainer from '../features/User/mypage/container/UserEditContain
 import MyPageContainer from '../features/User/mypage/container/MyPageContainer';
 import Header from '../shared/Header';
 import PostProductContainer from '../features/Product/PostProduct/container/PostProductContainer';
+import ProductDetailPage from '../pages/Product/ProductDetail/ProductDetailPage';
+import PageNotFound from '../pages/PageNotFound';
 
 export const RootRouter = () => {
   const location = useLocation();
 
   // 헤더를 표시할 경로를 정의합니다.
-  const showHeaderPaths = ['/', '/mypage', '/postproduct'];
+  const showHeaderPaths = ['/', '/mypage', '/postproduct', '/productdetail'];
   return (
     <>
       {showHeaderPaths.includes(location.pathname) && <Header />}
@@ -22,6 +24,8 @@ export const RootRouter = () => {
         <Route path="/mypage" element={<MyPageContainer />} />
         <Route path="/mypageEdit" element={<UserEditContainer />} />
         <Route path="/postproduct" element={<PostProductContainer />} />
+        <Route path="/productdetail" element={<ProductDetailPage />} />
+        <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </>
   );
