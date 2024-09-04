1a44648a822e97386b149ed2423818cce44ac3bb
aixion1506 <aixion1506@gmail.com>
2024-09-04 16:27:10 +0900

    feat: RootRouter에서 Header의 조건부 렌더링 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { RootRouter } from './app/rootRouter';
-import Header from './shared/Header';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
         <RootRouter />
       </BrowserRouter>
     </div>
--- src/app/rootRouter.js
+++ src/app/rootRouter.js
@@ -1,20 +1,28 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 import PostProductPage from '../features/Product/PostProduct/container/PostProductPage';
 import RegisterContainer from '../features/User/Register/container/RegisterContainer';
 import { LoginContainer } from '../features/User/login/container/LoginContainer';
 import UserEditContainer from '../features/User/mypage/container/UserEditContainer';
 import MyPageContainer from '../features/User/mypage/container/MyPageContainer';
+import Header from '../shared/Header';
 
 export const RootRouter = () => {
+  const location = useLocation();
+
+  // 헤더를 표시할 경로를 정의합니다.
+  const showHeaderPaths = ['/', '/mypage', '/postproduct'];
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/login" element={<LoginContainer />} />
-      <Route path="/register" element={<RegisterContainer />} />
-      <Route path="/mypage" element={<MyPageContainer />} />
-      <Route path="/mypageEdit" element={<UserEditContainer />} />
-      <Route path="/postproduct" element={<PostProductPage />} />
-    </Routes>
+    <>
+      {showHeaderPaths.includes(location.pathname) && <Header />}
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginContainer />} />
+        <Route path="/register" element={<RegisterContainer />} />
+        <Route path="/mypage" element={<MyPageContainer />} />
+        <Route path="/mypageEdit" element={<UserEditContainer />} />
+        <Route path="/postproduct" element={<PostProductPage />} />
+      </Routes>
+    </>
   );
 };
