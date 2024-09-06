38593324d608b5168cb90723af0e38096ffb28e2
aixion1506 <aixion1506@gmail.com>
2024-09-06 13:44:25 +0900

    fix: userReducer

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import productReducer from '../features/Product/store/proudctSlice';
 import mypageReducer from '../features/User/store/myPageSlice';
+import userReducer from '../features/User/store/userSlice';
 
 const rootReducer = combineReducers({
   product: productReducer,
