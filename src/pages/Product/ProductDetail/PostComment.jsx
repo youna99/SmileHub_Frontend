c9bc171093d224ca92e7b35547253d52e51389ad
aixion1506 <aixion1506@gmail.com>
2024-09-10 15:07:58 +0900

    Feat: PostComment axios

--- src/pages/Product/ProductDetail/PostComment.jsx
+++ src/pages/Product/ProductDetail/PostComment.jsx
@@ -1,10 +1,39 @@
+import axios from 'axios';
 import React from 'react';
 
 export default function PostComment() {
+  const submitComment = async (data) => {
+    try {
+      const res = axios.post('http://localhost:8000/review', {
+        productId: data.productId,
+        sellerId: 2,
+        reviewContent: '리뷰 시작',
+        reviewScore: 0,
+      });
+      console.log('응답 >>>>> ', res);
+    } catch (error) {
+      console.error('Error posting product:', error);
+      if (error.response) {
+        console.error('서버 응답:', error.response.data);
+      }
+    }
+  };
+  // {
+  //   "productId": "any",
+  //   "sellerId": "any",
+  //   "reviewContent": "any",
+  //   "reviewScore": "any"
+  // }
+
   return (
     <>
       <span class="font-semibold mr-2 text-lg">댓글</span>
-      <form id="commentForm" name="comment" class="space-y-4 mt-4">
+      <form
+        id="commentForm"
+        name="comment"
+        onSubmit={submitComment}
+        class="space-y-4 mt-4"
+      >
         <textarea
           id="comment"
           name="text"
