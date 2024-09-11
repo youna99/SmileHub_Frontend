f6a808bf5f49653b6539c13277aab752b8556525
youna <dbsk0108@gmail.com>
2024-09-11 21:57:00 +0900

    style, feat: 상품상세페이지 UI, 안전결제 연결

--- src/pages/Product/ProductDetail/ProductDetailPage.jsx
+++ src/pages/Product/ProductDetail/ProductDetailPage.jsx
@@ -9,11 +9,8 @@ import { useNavigate } from 'react-router-dom';
 import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons';
 
-import '../../../App.css'; // CSS 파일을 따로 만들어서 import
-
 const ProductDetail = () => {
   const [product, setProduct] = useState(null);
-  const [fetchProduct, setFetchProduct] = useState(null);
   const [loading, setLoading] = useState(true); // 로딩 상태 추가
   const [error, setError] = useState(null); // 오류 상태 추가
   const [isLiked, setIsLiked] = useState(false); // 찜 상태 관리
@@ -21,18 +18,17 @@ const ProductDetail = () => {
   const productId = new URLSearchParams(window.location.search).get(
     'productId',
   ); // URL에서 productId 가져오기
+  const navigate = useNavigate();
 
   const toggleLike = () => {
     setIsLiked(!isLiked); // 찜 상태 토글
   };
 
-  const navigate = useNavigate();
-
-  const handlePayment = (productId) => {
-    navigate('/mypage/payment', { state: { productId } });
+  const handlePayment = () => {
+    navigate('/mypage/payment', { state: { productId, product } });
   };
 
-  const handleClickChat = (productId) => {
+  const handleClickChat = () => {
     navigate('/chat', { state: { productId } });
   };
 
@@ -45,6 +41,7 @@ const ProductDetail = () => {
         setProduct(response.data); // 응답 데이터 저장
       } catch (error) {
         console.error('상품 데이터를 가져오는 중 오류 발생:', error);
+        setError('상품 데이터를 가져오는 중 오류 발생');
       } finally {
         setLoading(false); // 로딩 완료
       }
@@ -57,89 +54,112 @@ const ProductDetail = () => {
     return <div className="loading">로딩 중...</div>;
   }
 
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
   if (!product) {
     return <div className="error">상품을 찾을 수 없습니다.</div>;
   }
 
   return (
-    <div className="product-detail">
-      <h1 className="product-title ">{product.productName}</h1>
-      <section className="flex flex-col w-[44%] max-md:ml-0 max-md:w-full">
-        <div className="w-full aspect-square bg-zinc-100 max-md:px-5 max-md:pb-24 max-md:mt-5">
+    <div className="product-detail p-6 max-w-3xl mx-auto space-y-6">
+      <section className="flex flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-6">
+        <div className="w-full sm:w-1/2 aspect-w-1 aspect-h-1">
           <Swiper
             spaceBetween={50}
             slidesPerView={1}
             navigation
             pagination={{ clickable: true }}
-            modules={[Navigation, Pagination]} // modules 속성 추가
+            modules={[Navigation, Pagination]}
           >
             {product.images && product.images.length > 0 ? (
-              // {images.map((image, index) => (
               product.images.map((image, index) => (
                 <SwiperSlide key={index}>
                   <img
-                    key={index}
                     src={image}
                     alt={`상품 이미지 ${index + 1}`}
-                    className="product-image"
+                    className="object-cover w-full h-full"
                   />
                 </SwiperSlide>
               ))
             ) : (
-              <p>이미지가 없습니다.</p>
+              <p className="text-center">이미지가 없습니다.</p>
             )}
           </Swiper>
         </div>
-      </section>
-      <section className="content border-2 border-purple border-solid">
-        <div>
-          <span className="product-price">가격: {product.price} 원</span>
-          &nbsp;&nbsp;
-          <span className="product-status bg-[#FEE715] rounded shadow ">
-            {product.status}
-          </span>
+
+        <div className="flex flex-col space-y-4 sm:w-1/2">
+          <div className="flex justify-between mb-1">
+            <button className="bg-gray-200 text-black px-4 py-2 rounded hover:bg-[#FEE715] hover:text-black transition">
+              수정
+            </button>
+            <button className="bg-gray-200 text-black px-4 py-2 rounded hover:bg-red-500 hover:text-white transition">
+              삭제
+            </button>
+          </div>
+          <hr />
+          <h1 className="product-title text-2xl font-bold text-center mb-8 sm:text-3xl">
+            {product.productName}
+          </h1>
+          <div>
+            {/* 가격 및 상태 */}
+            <div className="flex justify-between items-center">
+              <span className="text-lg font-semibold">
+                가격: {product.price} 원
+              </span>
+              <span className="bg-green-500 text-white px-4 py-1 rounded">
+                {product.status}
+              </span>
+            </div>
+          </div>
+
+          <div className="flex flex-col sm:pt-24">
+            {/* 위치 정보 */}
+            <p className="text-gray-700 pb-2 sm:pb-5">
+              {product.location
+                ? `${product.location.depth1} ${product.location.depth2} ${product.location.depth3}`
+                : '위치 정보 없음'}
+            </p>
+
+            {/* 버튼 부분 */}
+            <div className="flex justify-between items-center space-x-4 sm:space-x-2">
+              <div className="flex items-center">
+                <button
+                  className="text-red-500 hover:text-red-700 transition duration-300"
+                  onClick={toggleLike}
+                >
+                  <FontAwesomeIcon
+                    icon={isLiked ? solidHeart : regularHeart}
+                    size="1x"
+                  />
+                </button>
+                <span className="ml-1">{product.totalLikes}</span>
+              </div>
+              <div className="flex space-x-2">
+                <button
+                  className="bg-[#FEE715] text-black px-4 py-2 rounded hover:bg-black hover:text-[#FEE715] transition"
+                  onClick={handleClickChat}
+                >
+                  채팅
+                </button>
+                <button
+                  className="bg-[#f3b105] text-white px-4 py-2 rounded hover:bg-red-600 transition"
+                  onClick={handlePayment}
+                >
+                  안전구매
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <p className="product-location">
-          위치:{' '}
-          {product.location
-            ? `${product.location.depth1} ${product.location.depth2} ${product.location.depth3}`
-            : '위치 정보 없음'}
-        </p>
-        {/* <p className="product-view-count">조회수: {product.viewCount}</p> */}
-        <div className="flex justify-evenly">
-          <button
-            onClick={toggleLike}
-            className="text-red-500 hover:text-red-700 transition-all duration-300"
-          >
-            <FontAwesomeIcon
-              icon={isLiked ? solidHeart : regularHeart}
-              size="2x"
-            />
-            {product.totalLikes}
-          </button>
-          <button
-            className="bg-[#FEE715] text-[#101820] px-4 py-2 rounded shadow hover:bg-yellow-600"
-            onClick={() => handleClickChat(productId)}
-          >
-            채팅
-          </button>
-          <button
-            className="bg-[#f3b105] text-[#ffefbc] py-2 rounded shadow hover:bg-red-600"
-            onClick={() => handlePayment(productId)}
-          >
-            안전구매
-          </button>
-        </div>
-        <p className="product-reports">
-          이 판매자의 신고 수: {product.totalReport}
-        </p>
-        <p className="product-created-at">
-          등록일: {new Date(product.createdAt).toLocaleDateString()}
-        </p>
       </section>
-      <section>
-        <p className="product-description">{product.content}</p>
+
+      <section className="mt-6">
+        <hr className="pb-5" />
+        <p className="text-gray-700">{product.content}</p>
       </section>
+
       <ProductTab />
     </div>
   );
