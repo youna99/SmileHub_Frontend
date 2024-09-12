ee99b87b185ddf221232c93354927dfa9fa508ab
youna <dbsk0108@gmail.com>
2024-09-12 19:07:32 +0900

    style: 상품상세페이지 최저가, userId에 따른 버튼 수정

--- src/pages/Product/ProductRead/ProductTab.jsx
+++ src/pages/Product/ProductRead/ProductTab.jsx
@@ -3,7 +3,7 @@ import PostComment from './PostComment';
 
 const ProductTab = (newItem) => {
   const newProductPriceRef = useRef(null); // 새상품 최저가
-  const newProductReviewRef = useRef(null); // 거래후기
+  // const newProductReviewRef = useRef(null); // 거래후기
   const userReviewRef = useRef(null); // 새상품 리뷰
 
   const handleScrollTo = (ref) => {
@@ -11,80 +11,107 @@ const ProductTab = (newItem) => {
       ref.current.scrollIntoView({ behavior: 'smooth' });
     }
   };
-  console.log(
-    'newItem >> ',
-    newItem.newItem.length,
-    // newItem.newItem.map((i, v) => console.log(i.title)),
-  );
+  console.log('newItem >> ', newItem.newItem.length);
 
   return (
     <div className="container mx-auto">
-      <div className="flex justify-evenly flex-wrap gap-5 sm:gap-10 px-5 sm:px-7 py-4 sm:py-8 mt-5 text-xl font-semibold sm:text-2xl text-black bg-zinc-100 rounded-lg">
-        <div
-          onClick={() => handleScrollTo(newProductPriceRef)}
-          className="cursor-pointer hover:text-[#f3b105] transition duration-300"
-        >
-          새상품 최저가
-        </div>
-        <div
+      <div
+        className="flex justify-evenly flex-wrap gap-5 sm:gap-10 px-5 sm:px-7 py-4 sm:py-8 mt-5 cursor-pointer text-xl font-semibold sm:text-2xl text-[#FEE715] hover:text-black bg-black hover:bg-[#FEE715] rounded-lg"
+        onClick={() => handleScrollTo(newProductPriceRef)}
+      >
+        <div>새상품 최저가</div>
+        {/* <div
           onClick={() => handleScrollTo(userReviewRef)}
           className="cursor-pointer hover:text-[#f3b105] transition duration-300"
         >
           거래후기
-        </div>
+        </div> */}
       </div>
-
       <div ref={newProductPriceRef} className="py-8">
-        <h2 className="text-xl sm:text-2xl font-bold text-gray-800">
-          게시글 상품과 연관된 상품입니다.
-        </h2>
-        <div className="mt-2 space-y-6">
-          {newItem.newItem && newItem.newItem.length > 0 ? (
-            newItem.newItem.map((item, index) => (
-              <div
-                key={index}
-                className="border p-4 rounded-lg shadow-md bg-white transition-transform transform hover:scale-105"
-              >
-                <a
-                  href={item.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-lg font-semibold text-blue-600 hover:text-blue-800"
-                >
-                  상품명:{' '}
-                  {item.title.split('<b>').join('').split('</b>').join('')}{' '}
-                  {/* <b> 태그 제거 */}
-                </a>
-                <p className="text-gray-700 mt-1">
-                  가격: <span className="font-bold">{item.lprice} 원</span>
-                </p>
-                <p className="text-gray-500">상점명: {item.mallName}</p>
-              </div>
-            ))
-          ) : (
-            <p className="text-center text-gray-500">
-              새 상품에 대한 정보가 없습니다.
-            </p>
-          )}
+        <div className="overflow-x-auto">
+          <table className="min-w-full table-auto border-collapse bg-white shadow-md rounded-lg">
+            <thead>
+              <tr className="bg-gray-100 border-b">
+                <th className="text-center p-2 text-gray-600 font-semibold w-1/4">
+                  판매처
+                </th>
+                <th className="text-center p-2 text-gray-600 font-semibold w-1/4">
+                  상품명
+                </th>
+                <th className="text-center p-2 text-gray-600 font-semibold w-1/4">
+                  가격
+                </th>
+                <th className="text-center p-2 text-gray-600 font-semibold w-1/4">
+                  링크
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {newItem.newItem && newItem.newItem.length > 0 ? (
+                newItem.newItem.map((item, index) => (
+                  <tr key={index} className="border-b hover:bg-gray-50">
+                    <td className="p-4 text-gray-800 w-1/4">
+                      {item.mallName}
+                      {item.npay ? (
+                        <span className="ml-2 bg-green-500 text-white px-2 py-1 text-xs rounded-full">
+                          Npay
+                        </span>
+                      ) : null}
+                    </td>
+                    <td className="p-4 text-gray-800 w-1/4">
+                      <span className="font-bold block sm:whitespace-normal sm:overflow-visible whitespace-nowrap overflow-hidden text-ellipsis max-w-[10ch] sm:max-w-[15ch]">
+                        {item.title
+                          .split('<b>')
+                          .join('')
+                          .split('</b>')
+                          .join('')
+                          .split('&amp;')
+                          .join('')
+                          .split('&quot')
+                          .join('')}
+                      </span>
+                    </td>
+                    <td className="p-4 text-red-500 w-1/4 text-center">
+                      {item.lprice} 원
+                    </td>
+                    <td className="p-4 w-1/4">
+                      <a
+                        href={item.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-600 hover:text-blue-800 font-semibold"
+                      >
+                        사러가기
+                      </a>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="4" className="text-center p-4 text-gray-500">
+                    새 상품에 대한 정보가 없습니다.
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
         </div>
-
-        <hr className="mt-2" />
       </div>
-      <div ref={userReviewRef} className="py-8">
+
+      {/* <hr className="mt-2" /> */}
+      {/* <div ref={userReviewRef} className="py-8">
         <h2 className="text-xl sm:text-2xl font-bold text-gray-800">
           거래후기
         </h2>
         <p className="mt-2 text-gray-600">
           여기에 거래 후기 내용이 들어갑니다.
         </p>
-        <hr className="mt-2" />
-
-        {/* 댓글 작성 폼 */}
-        <PostComment />
-
-        {/* 댓글 내역 */}
-        {/* <CommentSection /> */}
-      </div>
+        <hr className="mt-2" /> */}
+      {/* 댓글 작성 폼 */}
+      {/* <PostComment /> */}
+      {/* 댓글 내역 */}
+      {/* <CommentSection /> */}
+      {/* </div> */}
     </div>
   );
 };
